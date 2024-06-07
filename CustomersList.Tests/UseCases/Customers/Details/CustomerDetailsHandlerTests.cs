99f8f0fa924f7e5a1dd5b0fded17a59acac65ab4
Jean Paul Mir <jean.paul@bairesdev.com>
2024-06-07 09:45:20 -0400

    WIP: Unit Tests environment setup

---
+++ CustomersList.Tests/UseCases/Customers/Details/CustomerDetailsHandlerTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Moq.AutoMock;
+
+namespace CustomersList.Application.UseCases.Customers.Details.Tests
+{
+    public class CustomerDetailsHandlerTests
+    {
+        [Fact()]
+        public async Task GetCustomerDetailsForNonExistingCustomer_ReturnNotFoundResult()
+        {
+
+            // Arrange
+            var mocker = new AutoMocker();
+            var handler = mocker.CreateInstance<CustomerDetailsHandler>();
+            var request = new CustomerDetailsRequest( Guid.NewGuid().ToString() );
+
+            // Act
+            var result = await handler.ExecuteAsync(request, CancellationToken.None);
+            
+            // Assert
+            result.Should().NotBeNull();
+            result.Status.Should().Be(Ardalis.Result.ResultStatus.NotFound);   
+
+        }
+    }
+}
